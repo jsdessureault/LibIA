3e14bde36d7f703ee09a585161044d3ca2dd3807
Jean-Sébastien Dessureault <sdessureault@videotron.ca>
2020-09-28 14:57:07 -0400

    Update libIA.py

--- libIA.py
+++ libIA.py
@@ -9,12 +9,12 @@ from keras.utils import to_categorical
 from matplotlib import pyplot as plt
 import random
 
-import gitLibIA.chronometre as chrono
-import gitLibIA.perceptron
-import gitLibIA.regression
-import gitLibIA.regroupement
-import gitLibIA.apprentissage_q
-from gitLibIA.donnees import Donnees as donnees
+#import gitLibIA.chronometre as chrono
+#import gitLibIA.perceptron
+#import gitLibIA.regression
+#import gitLibIA.regroupement
+#import gitLibIA.apprentissage_q
+#from gitLibIA.donnees import Donnees as donnees
 
 import datetime
 
@@ -81,7 +81,7 @@ class LibIA:
     def __init__(self, bavard=True):
         self.bavard = bavard
         self.chrono = Chronometre(bavard)
-        self.donnees = Donnees()
+        #self.donnees = Donnees()
 
         if self.bavard:
             print("LibIA: Création de LibIA.")
