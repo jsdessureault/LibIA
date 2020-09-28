bc9a42eb68a2d96c15b3b844b7c4552ae5947043
Jean-Sébastien Dessureault <sdessureault@videotron.ca>
2020-09-28 10:40:12 -0400

    Update libIA.py

--- libIA.py
+++ libIA.py
@@ -55,8 +55,8 @@ class LibIA:
 
     def __init__(self, bavard=True):
         self.bavard = bavard
-        self.chrono = Chronometre(bavard)
-        self.donnees = Donnees()
+        self.chrono = chrono.Chronometre(bavard)
+        self.donnees = donnees.Donnees()
 
         if self.bavard:
             print("LibIA: Création de LibIA.")
